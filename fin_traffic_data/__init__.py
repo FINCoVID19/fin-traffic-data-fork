5f670dcc670d60796e2803e3d65b1133052a85da
Janne Solanpää <janne@solanpaa.fi>
2020-04-04 12:13:03 +0300

    Initial commit

---
+++ fin_traffic_data/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package for loading and aggregating traffic data from Traficom/Väylä.
+"""
+__author__ = 'Janne Solanpää'
+__author_email__ = 'janne@solanpaa.fi'
+__copyright__ = '© Janne Solanpää'
+__credits__ = ''
+__license__ = 'MIT'
+__version__ = '0.0.1'
+__mainainer__ = 'Janne Solanpää'
+__status__ = 'Alpha'
+__name__ = 'fin_traffic_data'
