0f6a8dc9475f5383b16c341de951cdc632d2929e
Janne Solanpää <janne@solanpaa.fi>
2020-04-12 13:50:50 +0300

    Add automatic appendign to aggregation

--- fin_traffic_data/aggregation.py
+++ fin_traffic_data/aggregation.py
@@ -6,6 +6,7 @@ import itertools
 import multiprocessing
 import re
 from typing import List, Text, Tuple
+import pathlib
 
 import pandas as pd
 import numpy as np
@@ -14,8 +15,28 @@ import tqdm
 
 from fin_traffic_data.utils import *
 
+# Info on TMS data
+_vehicle_categories = [1, 2, 3, 4, 5, 6, 7]
+_directions = [1, 2]
+_keys_default = np.array([r for r in itertools.product(_vehicle_categories, _directions)])
+
 
 def list_rawdata_files(path: Text):
+    """
+    List datafiles for the raw traffic data in the supplied directory.
+
+    Input
+    -----
+    path: Text
+        The datafile path
+
+    Returns
+    -------
+    List of tuples:
+        - filename
+        - begin date of the data in the file
+        - end date of the data in the file
+    """
     filenames = glob(f"{path}/fin_traffic_raw*.h5")
     files = []
     for f in filenames:
@@ -75,6 +96,7 @@ def check_all_dates_covered_by_raw_files(raw_files):
 
 
 def _tms_rawdata_dataframe_iterator(tms_num, raw_data_files):
+    """Iterator over the dataframe and dates of datafiles for raw data of the corresponding TMS"""
     for fileinfo in raw_data_files:
         try:
             df = pd.read_hdf(fileinfo[0], key=f"tms_{tms_num}", mode='r')
@@ -83,11 +105,6 @@ def _tms_rawdata_dataframe_iterator(tms_num, raw_data_files):
             yield (df, datetime_begin, datetime_end)
         except Exception as e:
             ...
-
-
-_vehicle_categories = [1, 2, 3, 4, 5, 6, 7]
-_directions = [1, 2]
-_keys_default = np.array([r for r in itertools.product(_vehicle_categories, _directions)])
 
 
 def _aggregate_single_timeinterval(tms_num, t_begin, t_end, delta_t, df, file_Tbegin, file_Tend, rawdata_iterator):
@@ -127,8 +144,24 @@ def _aggregate_single_timeinterval(tms_num, t_begin, t_end, delta_t, df, file_Tb
     return res, file_Tbegin, file_Tend
 
 
-def _aggregate_core(tms_num, t_begin, t_end, delta_t, raw_data_files) -> pd.DataFrame:
+def _aggregate_core(tms_num, t_begin, t_end, delta_t, raw_data_files, append_to_file) -> pd.DataFrame:
+    """
+    Aggregates all data on the TMS between two datetimes
 
+    Input
+    -----
+    tms_num: int
+        Number of the TMS station
+    t_begin: datetime.datetime
+        Begin time of the interval to aggregate
+    t_end: datetime.datetime
+        End time of the interval to aggregate
+    delta_t: datetime.timedelta
+        Time resolution
+    raw_data_files: Iterator
+        Iterator over the raw datafiles
+    append_to_file: Optional[Text]
+    """
     # Iterator for the dataframes containing raw data for this particular
     # measuring station
     rawdata_iterator = _tms_rawdata_dataframe_iterator(tms_num, raw_data_files)
@@ -146,48 +179,113 @@ def _aggregate_core(tms_num, t_begin, t_end, delta_t, raw_data_files) -> pd.Data
         tms_aggregated_dfs.append(_tmp_df)
     tms_df = pd.concat(tms_aggregated_dfs)
     with lock:
-        tms_df.to_hdf(
-            f'aggregated_data/fin-traffic-{delta_t}-{t_begin}-{t_end}.h5',
-            key=f'tms_{tms_num}',
-            complevel=9,
-            format='table',
-            nan_rep='None'
-        )
+        if append_to_file:
+            tms_df.to_hdf(
+                append_to_file, key=f'tms_{tms_num}', complevel=9, format='table', nan_rep='None', append=True
+            )
+
+        else:
+            tms_df.to_hdf(
+                f'aggregated_data/fin-traffic-{delta_t}-{t_begin}-{t_end}.h5',
+                key=f'tms_{tms_num}',
+                complevel=9,
+                format='table',
+                nan_rep='None'
+            )
 
 
-class Engine:
+class AggregationEngine:
 
-    def __init__(self, time0, time_end, delta_t, raw_data_files):
+    """Class for aggregating the raw data in a multiprocessing environment."""
+
+    def __init__(self, time0, time_end, delta_t, raw_data_files, append_to_file):
+        """
+        Input
+        -----
+        time0: datetime.datetime
+            Beginning of the time interval for aggregation
+        time_end: datetime.datetime
+            End of the time interval to aggregate
+        delta_t: datetime.timedelta
+            Time resolution
+        raw_data_files: List
+            List of the names of the datafiles for raw traffic data
+        """
         self.time0 = time0
         self.time_end = time_end
         self.delta_t = delta_t
         self.raw_data_files = raw_data_files
+        self.append_to_file = append_to_file
 
     def __call__(self, tms_num):
-        _aggregate_core(tms_num, self.time0, self.time_end, self.delta_t, self.raw_data_files)
+        """
+        Aggregates the data for a single TMS station
+
+        Input
+        -----
+        tms_num: int
+            Number of the TMS station
+        """
+        _aggregate_core(
+            tms_num, self.time0, self.time_end, self.delta_t, self.raw_data_files, self.append_to_file
+        )
 
 
-def init(l):
+def init(arg_lock):
+    """Initialization of the multiprocessing Pool with a lock for
+    filesaving operations."""
     global lock
-    lock = l
+    lock = arg_lock
 
 
 def aggregate_datafiles(
         raw_data_files: List[Tuple[Text, datetime.date, datetime.date]], all_tms_numbers: List[int],
         delta_t: datetime.timedelta
 ) -> pd.DataFrame:
-    first_date = min(raw_data_files, key=lambda f: f[1])[1]
-    last_date = max(raw_data_files, key=lambda f: f[2])[2]
 
-    time0 = datetime.datetime(year=first_date.year, month=first_date.month, day=first_date.day, hour=0, minute=0)
+    # Check if there is an existing aggregated datafile with the same resolution
 
-    time_end = datetime.datetime(year=last_date.year, month=last_date.month, day=last_date.day, hour=0, minute=0)
+    try:
+        aggregate_files = glob(f'aggregated_data/fin-traffic-{delta_t}-*.h5')
 
-    time = time0
+        def get_daterange(afilename):
+            m = re.match(
+                r"aggregated_data\/fin-traffic-" + f"{delta_t}" +
+                r"-(?P<begindate>\d{4}-\d{2}-\d{2})\s00:00:00-(?P<enddate>\d{4}-\d{2}-\d{2})\s00:00:00\.h5", afilename
+            )
+            d1 = [int(i) for i in m.group("begindate").split('-')]
+            d2 = [int(i) for i in m.group('enddate').split('-')]
+            return datetime.date(year=d1[0], month=d1[1], day=d1[2]), datetime.date(year=d2[0], month=d2[1], day=d2[2])
 
+        afile_dateranges = [(afile, get_daterange(afile)) for afile in aggregate_files]
+
+        # Filter out those files that do not have even partially overlapping data
+        first_date = min(raw_data_files, key=lambda f: f[1])[1]
+        afile_dateranges = list(filter(lambda s: s[1][0] <= first_date, afile_dateranges))
+
+        last_date = max(raw_data_files, key=lambda f: f[2])[2]
+
+        afile_dateranges = list(filter(lambda s: s[1][0] < last_date, afile_dateranges))
+        afile_to_append = max(afile_dateranges, key=lambda f: f[1][1])
+
+        afile_lastday = afile_to_append[1][1]
+        afile_to_append = afile_to_append[0]
+        time0 = datetime.datetime(
+            year=afile_lastday.year, month=afile_lastday.month, day=afile_lastday.day, hour=0, minute=0
+        )
+        time_end = datetime.datetime(year=last_date.year, month=last_date.month, day=last_date.day, hour=0, minute=0)
+        new_filename = f'aggregated_data/fin-traffic-{delta_t}-{afile_to_append[1][0]}-{time_end}.h5'
+    except Exception:
+        afile_to_append = None
+        time0 = datetime.datetime(year=first_date.year, month=first_date.month, day=first_date.day, hour=0, minute=0)
+        time_end = datetime.datetime(year=last_date.year, month=last_date.month, day=last_date.day, hour=0, minute=0)
     # Iterate over TMSs
-    lock = multiprocessing.Lock()
+    lock = multiprocessing.Lock()  # For locking data saving operations
     pool = multiprocessing.Pool(initializer=init, initargs=(lock, ))
-    engine = Engine(time0, time_end, delta_t, raw_data_files)
+    engine = AggregationEngine(time0, time_end, delta_t, raw_data_files, afile_to_append)
     for _ in tqdm.tqdm(pool.imap(engine, all_tms_numbers)):
         pass
+
+    if afile_to_append:
+        path = pathlib.Path(afile_to_append)
+        path.replace(new_filename)
--- fin_traffic_data/scripts/aggregate_raw_data.py
+++ fin_traffic_data/scripts/aggregate_raw_data.py
@@ -1,15 +1,14 @@
 import argparse
-from datetime import date, timedelta
+from datetime import timedelta
 import pathlib
 import re
-
-import progressbar
 
 from fin_traffic_data.metadata import *
 from fin_traffic_data.aggregation import *
 from fin_traffic_data.utils import daterange
 
 def _parse_time_resolution(x):
+    """Parse human-readable input of time resolution"""
     m = re.findall(r"(?P<num>\d+)(?P<qualif>w|d|h|m|s)", x)
     dt = timedelta()
     for obj in m:
@@ -29,7 +28,7 @@ def _parse_time_resolution(x):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Aggregates pre-loaded traffic data")
+        description="Aggregates pre-loaded raw traffic data.")
     parser.add_argument("--dir", type=str,
                         help="Directory containing the raw traffic data",
                         default="raw_data")
@@ -43,17 +42,22 @@ def main():
     basepath = args.dir
     delta_t = args.time_resolution
 
+    # Create the output directory
     pathlib.Path("aggregated_data/").mkdir(parents=True, exist_ok=True)
 
+    # Get the raw datafiles
     raw_data_files = list_rawdata_files(basepath)
     if not raw_data_files:
         raise RuntimeError(f"No datafiles in {basepath}/")
 
+    # Sanity checking of the files with raw traffic data
     check_no_daterange_overlap_in_raw_files(raw_data_files)
     check_all_dates_covered_by_raw_files(raw_data_files)
 
+    # Get list of all TMS stations
     all_tms_stations = get_tms_stations()
 
+    # Aggregate all the datafiles
     aggregate_datafiles(
         raw_data_files,
         all_tms_stations['num'],
